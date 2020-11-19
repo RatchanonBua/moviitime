3228a9dab617d0e4af7e15c0724ace508f99375c
RatchanonBua <ratchanon.blssk@gmail.com>
2020-11-19 22:23:35 +0700

    Homepage mockup.

---
+++ resources/views/layouts/_typebar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar-expand-lg" id="sidebar">
+    {{-- Header --}}
+    <div class="collapse navbar-collapse">
+        <div class="sidebar-header"><h5>Category</h5></div>
+    </div>
+    {{-- List --}}
+    <div class="collapse navbar-collapse">
+        <ul class="list-unstyled components">
+            <li class="active"><a href="#">All</a></li>
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Adventure</a></li>
+            <li><a href="#">Animation</a></li>
+            <li><a href="#">Comedy</a></li>
+            <li><a href="#">Crime</a></li>
+            <li><a href="#">Documentaries</a></li>
+            <li><a href="#">Drama</a></li>
+            <li><a href="#">Family</a></li>
+            <li><a href="#">Horror</a></li>
+            <li><a href="#">Musicals</a></li>
+            <li><a href="#">Romance</a></li>
+            <li><a href="#">Sci-Fi </a></li>
+            <li><a href="#">Thriller</a></li>
+            <li><a href="#">War</a></li>
+        </ul>
+    </div>
+</nav>
